018b79810d08d43d722114d1ea8c7db5bb8a9c1a
Nathan Witmer <nathan@zerowidth.com>
2024-01-05 16:39:13 -0700

    Add generated item along with configured shorthand

--- github-shorthand/src/gh-shorthand.tsx
+++ github-shorthand/src/gh-shorthand.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import fs from "fs";
 import yaml from "js-yaml";
 import { configPath } from "./utils";
-import { ActionPanel, Action, Detail, List, showToast, Toast } from "@raycast/api";
+import { ActionPanel, Action, List, showToast, Toast } from "@raycast/api";
 
 type Config = {
   users: { [shorthand: string]: string };
@@ -37,18 +37,34 @@ export default function Main() {
   }
 
   function FullList() {
+    const [searchText, setSearchText] = useState("");
+    const exactMatch = Object.entries(config.users).some(([shorthand,]) => shorthand == searchText);
     return (
-      <List>
+      <List searchText={searchText} onSearchTextChange={setSearchText} filtering={true}
+      searchBarPlaceholder="Shorthand or user...">
         {Object.keys(config.users).length > 0 && (
           <List.Section title="Users">
+            {!exactMatch && searchText.length > 0 && !searchText.includes("/") && (
+              <List.Item
+                key={`search-${searchText}`}
+                title={`${searchText}`}
+                subtitle={searchText + "/..."}
+                actions={
+                  <ActionPanel>
+                    <Action.Push title="View Repositories" target={<RepoList owner={searchText} />} />
+                  </ActionPanel>
+                }
+              />
+            )}
             {Object.entries(config.users).map(([shorthand, full]) => (
               <List.Item
+                key={shorthand}
                 title={`${shorthand}/`}
-                subtitle={full}
+                subtitle={full + "/..."}
                 keywords={[full]}
                 actions={
                   <ActionPanel>
-                    <Action.Push title="View Repositories" target={<RepoList user={full} />} />
+                    <Action.Push title="View Repositories" target={<RepoList owner={full} />} />
                   </ActionPanel>
                 }
               />
@@ -58,7 +74,7 @@ export default function Main() {
         {Object.keys(config.repos).length > 0 && (
           <List.Section title="Repositories">
             {Object.entries(config.repos).map(([shorthand, full]) => (
-              <List.Item title={shorthand} subtitle={full} keywords={full.split("/")} />
+              <List.Item key={shorthand} title={shorthand} subtitle={full} keywords={full.split("/")} />
             ))}
           </List.Section>
         )}
@@ -67,43 +83,28 @@ export default function Main() {
   }
 
   interface RepoListProps {
-    user: string;
+    owner: string;
   }
 
-  function RepoList({ user: user }: RepoListProps) {
+  function RepoList({ owner: owner }: RepoListProps) {
     const [searchText, setSearchText] = useState("");
-    const items = Object.entries(config.repos).filter(([_, full]) => {
-      return full.split("/")[0] == user;
+    const items = Object.entries(config.repos).filter(([, full]) => {
+      return full.split("/")[0] == owner;
     });
     useEffect(() => {
       items.filter(([, full]) => {
         return full.includes(searchText);
-      })
+      });
     }, [searchText]);
+    const exactMatch = Object.entries(items).some(([, [shorthand]]) => shorthand == searchText);
     return (
-      <List
-        searchText={searchText}
-        onSearchTextChange={setSearchText}
-        // filtering={true}
-      >
-        {Object.entries(items).length == 0 ? (
-          <List>
-            <List.Item title={`No repositories found for ${searchText}`} />
-          </List>
-        ) : (
-          <List.Section title={`Repositories for ${user}`}>
-            {Object.entries(items).map(([, [shorthand, full]]) => (
-              <List.Item title={shorthand} subtitle={full} keywords={[full]} />
-            ))}
-          </List.Section>
-        )}
-        ;
+      <List searchText={searchText} onSearchTextChange={setSearchText} filtering={true}>
+        {searchText.length > 0 && !exactMatch && <List.Item key={`search-${searchText}`} title={`${owner}/${searchText}`} />}
+        {Object.entries(items).map(([, [shorthand, full]]) => (
+          <List.Item key={shorthand} title={shorthand} subtitle={full} keywords={full.split("/")} />
+        ))}
       </List>
     );
-  }
-
-  function RepoView() {
-    return <List></List>;
   }
 
   return <FullList />;
